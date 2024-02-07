11fb32093afe8979047bb8287cf781f8282c4145
Eyup Cakli <eyupcakli@hotmail.com>
2024-02-07 11:03:07 +0300

    feat(components/model): Model class created.

---
+++ src/app/core/components/admin/model/models/model.ts
@@ -0,0 +1,10 @@
+export class Model {
+    id: Number;
+    name: string;
+    lengthMeter: Number;
+    beamMeter: Number;
+    draftMeter: Number;
+    manufacturer: any;
+    type: any;
+    hull_material: any;
+}
